3b2434d3a13fbaff6374e04d9cd74853c0c9d3a7
Michael Caira <mbcaira@gmail.com>
2024-09-30 22:11:50 -0400

    Add first opcodes

---
+++ chip8.cc
@@ -0,0 +1,250 @@
+//
+// Created by mbcai on 2024-09-30.
+//
+#include "chip8.h"
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+
+/*
+ * Initializes program counter and fontset
+ */
+chip8::chip8() : randGen(std::chrono::system_clock::now().time_since_epoch().count()) {
+    pc = START_ADDRESS;
+    // 16 characters at 5 bytes each
+    for (unsigned int i = 0; i < FONTSET_SIZE; i++) {
+        memory[FONTSET_START_ADDRESS + i] = fontset[i];
+    }
+
+    randByte = std::uniform_int_distribution<uint8_t>(0, 255U);
+}
+
+
+void chip8::LoadRom(const char *filename) {
+
+    // Open ROM file and move pointer to the end
+    std::ifstream file(filename, std::ios::binary | std::ios::ate);
+
+    if (file.is_open()) {
+        // Allocate a buffer to hold the ROM content
+        std::streampos size = file.tellg();
+        char *buffer = new char[size];
+
+        //        Fill the buffer
+        file.seekg(0, std::ios::beg);
+        file.read(buffer, size);
+        file.close();
+
+        for (long i = 0; i < size; i++) {
+            memory[START_ADDRESS + i] = buffer[i];
+        }
+
+        delete[] buffer;
+    }
+}
+
+void chip8::OP_00E0() {
+    memset(video, 0, sizeof(video));
+}
+
+void chip8::OP_00EE() {
+    --sp;
+    pc = stack[sp];
+}
+
+void chip8::OP_1nnn() {
+    uint16_t address = opcode & 0xFFFu;
+    pc = address;
+}
+
+void chip8::OP_2nnn() {
+    uint16_t address = opcode & 0xFFFu;
+    stack[sp] = pc;
+    ++sp;
+    pc = address;
+}
+
+void chip8::OP_3xkk() {
+    uint8_t Vx = (opcode & 0xF00u) >> 8u;
+    uint8_t byte = opcode & 0x00Fu;
+
+    if (registers[Vx] == byte) {
+        pc += 2;
+    }
+}
+
+void chip8::OP_4xkk() {
+    uint8_t Vx = (opcode & 0XF00u) >> 8u;
+    uint8_t byte = opcode && 0x00FFu;
+
+    if (registers[Vx] != byte) {
+        pc += 2;
+    }
+}
+
+void chip8::OP_5xy0() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy (opcode && 0x00F0u) >> 4u;
+
+    if (registers[Vx] == registers[Vy]) {
+        pc += 2;
+    }
+}
+
+void chip8::OP_6xkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    registers[Vx] = byte;
+}
+
+void chip8::OP_7xkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    registers[Vx] += byte;
+}
+
+void chip8::OP_8xy0() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] = registers[Vy];
+}
+
+void chip8::OP_8xy1() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] |= registers[Vy];
+}
+
+void chip8::OP_8xy2() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] &= registers[Vy];
+}
+
+void chip8::OP_xy3() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    registers[Vx] ^= registers[Vy];
+}
+
+void chip8::OP_8xy4() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    uint16_t sum = registers[Vx] + registers[Vy];
+
+    if (sum > 255U) {
+        registers[0xF] = 1;
+    } else {
+        registers[0xF] = 0;
+    }
+
+    registers[Vx] = sum & 0xFFu;
+}
+
+void chip8::OP_8xy5() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vx] > registers[Vy]) {
+        registers[0xF] = 1;
+    } else {
+        registers[0xF] = 0;
+    }
+
+    registers[Vx] -= registers[Vy];
+}
+
+void chip8::OP_8xy6() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    registers[0xF] = (registers[Vx] & 0x1u);
+    registers[Vx] >>= 1;
+}
+
+void chip8::OP_8xy7() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vy] > registers[Vx]) {
+        registers[0xF] = 1;
+    } else {
+        registers[0xF] = 0;
+    }
+
+    registers[Vx] = registers[Vy] - registers[Vx];
+}
+
+void chip8::OP_8xyE() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+
+    registers[0xF] = (registers[Vx] & 0x80u) >> 7u;
+
+    registers[Vx] <<= 1;
+}
+
+void chip8::OP_9xy0() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+
+    if (registers[Vx] != registers[Vy]) {
+        pc += 2;
+    }
+}
+
+void chip8::OP_Ann() {
+    uint16_t address = opcode & 0X0FFFu;
+
+    index = address;
+}
+
+void chip8::OP_Bnn() {
+    uint16_t address = opcode & 0x0FFFu;
+
+    pc = registers[0] + address;
+}
+
+void chip8::OP_Cxkk() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t byte = opcode & 0x00FFu;
+
+    registers[Vx] = randByte(randGen) & byte;
+}
+
+void chip8::OP_Dxyn() {
+    uint8_t Vx = (opcode & 0x0F00u) >> 8u;
+    uint8_t Vy = (opcode & 0x00F0u) >> 4u;
+    uint8_t height = opcode & 0x000Fu;
+
+    // Wrap if past screen boundaries
+    uint8_t xPos = registers[Vx] % VIDEO_WIDTH;
+    uint8_t yPos = registers[Vy] % VIDEO_HEIGHT;
+
+    registers[0xF] = 0;
+
+    // Loop over the sprite and check if there's a collision and set VF
+    // XOR screen pixel with 0xFFFFFFFF to essentially XOR it with sprite pixel since it's either 0 or 1 and can't be done directly
+    for (unsigned int row = 0; row < height; row++) {
+        uint8_t spriteByte = memory[index + row];
+
+        for (unsigned int col = 0; col < 8; col++) {
+            uint8_t spritePixel = spriteByte & (0x80u >> col);
+            uint32_t* screenPixel = &video[(yPos + row) * VIDEO_WIDTH + (xPos + col)];
+
+            // Sprite pixel is on
+            if (spritePixel) {
+                // Screen pixel is on, collision set
+                if (*screenPixel == 0xFFFFFFFF) {
+                    registers[0xF] = 1;
+                }
+
+                *screenPixel ^= 0xFFFFFFFF;
+            }
+        }
+    }
+}
---
+++ chip8.h
@@ -0,0 +1,135 @@
+//
+// Created by mbcai on 2024-09-30.
+//
+
+#ifndef CHIP8_EMULATOR_CHIP8_H
+#define CHIP8_EMULATOR_CHIP8_H
+
+#include <chrono>
+#include <cstdint>
+#include <random>
+
+class chip8 {
+private:
+    static const unsigned int START_ADDRESS = 0x200;
+    static const unsigned int FONTSET_START_ADDRESS = 0x50;
+    static const unsigned int FONTSET_SIZE = 80;
+
+    uint8_t registers[16]{};
+    uint8_t memory[4096]{};
+    uint16_t index{};
+    uint16_t pc{};
+    uint16_t stack[16]{};
+    uint8_t sp{};
+    uint8_t delayTimer{};
+    uint8_t soundTimer{};
+    uint8_t keypad[16]{};
+    uint32_t video[64 * 32]{};
+    uint16_t opcode;
+    uint8_t fontset[FONTSET_SIZE] = {
+            0xF0, 0x90, 0x90, 0x90, 0xF0,// 0
+            0x20, 0x60, 0x20, 0x20, 0x70,// 1
+            0xF0, 0x10, 0xF0, 0x80, 0xF0,// 2
+            0xF0, 0x10, 0xF0, 0x10, 0xF0,// 3
+            0x90, 0x90, 0xF0, 0x10, 0x10,// 4
+            0xF0, 0x80, 0xF0, 0x10, 0xF0,// 5
+            0xF0, 0x80, 0xF0, 0x90, 0xF0,// 6
+            0xF0, 0x10, 0x20, 0x40, 0x40,// 7
+            0xF0, 0x90, 0xF0, 0x90, 0xF0,// 8
+            0xF0, 0x90, 0xF0, 0x10, 0xF0,// 9
+            0xF0, 0x90, 0xF0, 0x90, 0x90,// A
+            0xE0, 0x90, 0xE0, 0x90, 0xE0,// B
+            0xF0, 0x80, 0x80, 0x80, 0xF0,// C
+            0xE0, 0x90, 0x90, 0x90, 0xE0,// D
+            0xF0, 0x80, 0xF0, 0x80, 0xF0,// E
+            0xF0, 0x80, 0xF0, 0x80, 0x80 // F
+    };
+
+    std::default_random_engine randGen;
+    std::uniform_int_distribution<uint8_t> randByte;
+
+
+    chip8();
+
+public:
+    // Loads ROM into memory
+    void LoadRom(char const *filename);
+
+    // Clear display
+    void OP_00E0();
+
+    // Return from a subroutine
+    void OP_00EE();
+
+    // Jump to location nnn
+    void OP_1nnn();
+
+    // Call subroutine at nnn
+    void OP_2nnn();
+
+    // Skip next instruction if Vx == kk
+    void OP_3xkk();
+
+    // Skip next instruction if Vx != kk
+    void OP_4xkk();
+
+    // Skip next instruction if Vx = Vy
+    void OP_5xy0();
+
+    // Set Vx = kk
+    void OP_6xkk();
+
+    // Set Vx = vx + kk
+    void OP_7xkk();
+
+    // Set Vx = Vy
+    void OP_8xy0();
+
+    // Set Vx = Vx OR Vy
+    void OP_8xy1();
+
+    // Set Vx = Vx AND Vy
+    void OP_8xy2();
+
+    // Set Vx = Vx XOR Vy
+    void OP_xy3();
+
+    /*
+     * Set Vx = Vx + Vy, VF = carry
+     * Vx will be only the lowest 8 bits if overflow occurs
+     */
+    void OP_8xy4();
+
+    // Set Vx = Vx - Vy, set VF = NOT borrow
+    void OP_8xy5();
+
+    // Set Vx = Vx SHR 1, LSB saved to VF
+    void OP_8xy6();
+
+    // Set Vx = Vy - Vx, set VF not borrow
+    void OP_8xy7();
+
+    // Set Vx = Vx SHL 1, VF set to 1 if MSB is 1
+    void OP_8xyE();
+
+    // Skip next instruction if Vx != Vy
+    void OP_9xy0();
+
+    // Set I = nnn
+    void OP_Ann();
+
+    // Jump to location nnn + V0
+    void OP_Bnn();
+
+    // Set Vx = random byte AND kk
+    void OP_Cxkk();
+
+    /*
+     * Display n-byte sprite at memory location I at (Vx, Vy)
+     * VF = collision
+     */
+    void OP_Dxyn();
+};
+
+
+#endif//CHIP8_EMULATOR_CHIP8_H
